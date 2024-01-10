b66ef42dfc6ca901aec6ebec76763985a3ad574d
DA <91001298+mintss-zez0@users.noreply.github.com>
2024-01-10 23:49:31 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sky.mk
+    $(LOCAL_DIR)/cherish_sky.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sky-eng \
-    aosp_sky-userdebug \
-    aosp_sky-user
+    cherish_sky-user \
+    cherish_sky-userdebug \
+    cherish_sky-eng
